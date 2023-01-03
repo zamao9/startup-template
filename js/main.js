da36d10ee7cd34fa111404060483f9b559be8c65
Zama <zama.barsaev@gmail.com>
2023-01-04 01:03:06 +0300

    asasfa

--- js/main.js
+++ js/main.js
@@ -3,14 +3,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const activeBurger = () => {
     const burger = document.querySelector(".header__burger"),
       defaultItems = document.querySelector(".header__items-default"),
-      activeItems = document.querySelector(".header__items-active");
+      activeItems = document.querySelector(".header__items-active"),
+      nav = document.querySelector(".nav"),
+      background = document.querySelector(".header__background"),
+      body = document.getElementsByTagName("body");
 
     burger.addEventListener("click", () => {
       defaultItems.classList.toggle("active");
       activeItems.classList.toggle("active");
+      nav.classList.toggle("active");
+      background.classList.toggle("active");
+      body[0].classList.toggle("active");
+    });
+
+    background.addEventListener("click", (e) => {
+      e.currentTarget.classList.remove("active");
+      defaultItems.classList.remove("active");
+      activeItems.classList.remove("active");
+      nav.classList.remove("active");
+      body[0].classList.remove("active");
     });
   };
   activeBurger();
+
+  /* Header Active */
+  window.onscroll = () => {
+    const header = document.querySelector(".header__head"),
+      scrollY = window.scrollY;
+
+    if (scrollY > 10) {
+      header.classList.add("active");
+    }
+    if (scrollY < 10) {
+      header.classList.remove("active");
+    }
+  };
 
   /* Clients Swiper */
   const clientsSwiper = new Swiper(".clients__swiper", {
